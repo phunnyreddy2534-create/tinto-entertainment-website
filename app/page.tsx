f67da00528a07d0f4f3e4f7af4d012f1292cfad2
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:19:24 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Tinto Entertainment</h1>
+      <p>Premium Event & Entertainment Company</p>
+    </main>
+  )
+}
