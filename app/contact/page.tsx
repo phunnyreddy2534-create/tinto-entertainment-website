ba3b292d804f2026c817775cd3d5ee41270068ba
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:27:38 +0530

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h2>Contact</h2>
+      <p>Phone: +91XXXXXXXXXX</p>
+      <p>Email: tinto@email.com</p>
+      <p>Instagram: @tintoentertainment</p>
+    </main>
+  )
+}
