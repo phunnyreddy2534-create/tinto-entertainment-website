f0fc6e8a4a71bf8e534448d64e0f4c32e06d9cc1
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:35:54 +0530

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: 20, textAlign: "center", opacity: 0.7 }}>
+      © {new Date().getFullYear()} Tinto Entertainment. All rights reserved.
+    </footer>
+  )
+}
