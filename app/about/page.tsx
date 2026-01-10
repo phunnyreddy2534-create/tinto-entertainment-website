b347368b7439b1ac059d2923d89554b64dc5e85d
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:20:59 +0530

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h2>About Us</h2>
+      <p>We organize concerts, festivals, weddings and premium events.</p>
+      <h3>Our Team</h3>
+      <p>Professional event planners & creatives.</p>
+    </main>
+  )
+}
