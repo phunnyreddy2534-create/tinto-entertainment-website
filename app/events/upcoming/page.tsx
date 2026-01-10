8a1aef46707c90ea36db8f0e97564c36ab5ed6f6
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:33:52 +0530

    Create page.tsx

---
+++ app/events/upcoming/page.tsx
@@ -0,0 +1,8 @@
+export default function UpcomingEvents() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h2>Upcoming Events</h2>
+      <p>All upcoming events will appear here.</p>
+    </main>
+  )
+}
