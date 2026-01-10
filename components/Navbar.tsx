f084ea8fdd628e85f67e817271500fd5b6426617
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:26:52 +0530

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav style={{ padding: 16 }}>
+      <a href="/">Home</a> | <a href="/about">About</a> |{" "}
+      <a href="/events/upcoming">Upcoming</a> |{" "}
+      <a href="/events/past">Past</a> |{" "}
+      <a href="/contact">Contact</a>
+    </nav>
+  )
+}
