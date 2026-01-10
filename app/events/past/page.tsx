8726204e701e31fd18fe770990a9e6a31305cd74
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 20:34:36 +0530

    Create page.tsx

---
+++ app/events/past/page.tsx
@@ -0,0 +1,8 @@
+export default function PastEvents() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h2>Past Events</h2>
+      <p>Our successfully completed events.</p>
+    </main>
+  )
+}
