f8a9cbc3e76f347eee359a5a4232055039c03086
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 22:22:28 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link"
+
 export default function Navbar() {
   return (
-    <nav style={{ padding: 16 }}>
-      <a href="/">Home</a> | <a href="/about">About</a> |{" "}
-      <a href="/events/upcoming">Upcoming</a> |{" "}
-      <a href="/events/past">Past</a> |{" "}
-      <a href="/contact">Contact</a>
+    <nav style={{
+      padding: "16px",
+      textAlign: "center",
+      fontWeight: 600
+    }}>
+      <Link href="/">Home</Link>{" | "}
+      <Link href="/about">About</Link>{" | "}
+      <Link href="/upcoming">Upcoming</Link>{" | "}
+      <Link href="/past">Past</Link>
     </nav>
   )
 }
