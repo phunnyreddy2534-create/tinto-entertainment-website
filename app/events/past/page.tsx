a9b1b37e9a77268438ff7fb8a3d69b65b4dcb081
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 22:55:07 +0530

    Delete app/events directory

--- app/events/past/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-static";
-export default function Past() {
-  return (
-    <div style={{ padding: 30 }}>
-      <h1>Past Events</h1>
-      <p>Our successfully completed premium events.</p>
-    </div>
-  );
-}
