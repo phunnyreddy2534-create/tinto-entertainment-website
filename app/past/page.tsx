9b07ae40d773fc53c42cada463442865f47811bd
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-10 22:56:26 +0530

    Create page.tsx

---
+++ app/past/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-static";
+export default function Past() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Past Events</h1>
+      <p>Our successfully completed premium events.</p>
+    </div>
+  );
+}
