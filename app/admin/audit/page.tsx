6f697c5bb3c286677d67e2e4747318cd0f9f656b
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-11 16:44:57 +0530

    Update page.tsx

--- app/admin/audit/page.tsx
+++ app/admin/audit/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { useBrand } from "../../lib/useBrand"
+import { useBrand } from "../../../lib/useBrand"
 
 type Log = {
   time: string
